b1cb8e4a1ce6bf4dc52a0cb0dcdb356fd6d35ea4
Hamid Hussian <hamidhussian@MP15-HamidH-SE-363.local>
2022-03-23 05:50:16 +0500

    login signup slices created

--- src/store/slices/login-slice.js
+++ src/store/slices/login-slice.js
@@ -1,6 +1,4 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
-// import movieApi from '../../common/apis/movieApi'
-// import { APIKey } from '../../common/apis/movieApiKey'
 import axios from 'axios'
 
 
@@ -8,29 +6,13 @@ import axios from 'axios'
 export const loginAsync = createAsyncThunk('login/loginAsync', async(payload)=>{
 
     console.log("loginasync",payload)
-    // const movieText = "Harry"
-        // i=tt3896198&apikey=1efb613b
+
     const response = await axios.post(`http://127.0.0.1:3000/api/user/login`,{email:payload.email,password:payload.password})
     console.log(response)
     return response.data
-    // dispatch(addMovies(response.data))
 
     
 })
-
-// export const fetchAsyncShows = createAsyncThunk('movies/fetchAsyncShows', async()=>{
-
-
-//     const seriesText = "Friends"
-//         // i=tt3896198&apikey=1efb613b
-//     const response = await movieApi.get(`?i=tt3896198&apikey=${APIKey}&s=${seriesText}&type=series`)
-//     console.log(response)
-//     return response.data
-//     // dispatch(addMovies(response.data))
-
-    
-// })
-
 
 const initialState = {
     user:{},
@@ -59,15 +41,9 @@ const loginSlice = createSlice({
             console.log("Rejected");
 
         },
-        // [fetchAsyncShows.fulfilled]:(state,{payload})=>{
-        //     console.log("Fetched Successfully")
-        //     return {...state, shows:payload}
-
-        // },
+       
     }
 })
 
-// export const {addMovies} = loginSlice.actions
-// export const getAllMovies = (state)=>state.movies.movies
 export const getLoginResponse = (state)=>state.loginResponse
 export default loginSlice.reducer;
