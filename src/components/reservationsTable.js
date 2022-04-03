c6e1cf0e244249b36af1e710b3662432c8b870cb
Hamid Hussian <hamidhussian@MP15-HamidH-SE-363.local>
2022-04-03 23:48:54 +0500

    changes

--- src/components/reservationsTable.js
+++ src/components/reservationsTable.js
@@ -91,6 +91,7 @@ export default function StickyHeadTable() {
                 <TableCell
                   key={column.id}
                   align={column.align}
+                  
                   style={{ minWidth: column.minWidth }}
                 >
                   {column.label}
